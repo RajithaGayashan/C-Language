fce9e117011243af42db897b26c002ccfa193fee
RajithaGayashan <rajithagayashan95@gmail.com>
2019-06-16 08:25:35 -1200

    Conversational charactors

---
+++ Test2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+    printf("My name is %s %s\n","Rajitha","Gayashan");
+    printf("My age is %d\n",23);
+    printf("%c\n",'A');
+    printf("%f\n",1.5678);
+    printf("%.1f\n",1.5678);
+    printf("%.2f\n",1.5678);
+    printf("%.3f\n",1.5678);
+    
+}
