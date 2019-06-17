7a3bfd794af6ae1e4d034033a9eb2d8c1b68b47e
RajithaGayashan <rajithagayashan95@gmail.com>
2019-06-17 07:42:41 -1200

    Integer variable type

---
+++ Test3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+    int a=10;
+    int b=30;
+    int sub=a+b;
+    printf("Total of 10 and 30 :  %d\n",a+b);
+    printf("Sub of 10 and 30   :  %d\n",a-b);
+    printf("mul of 10 and 30   :  %d\n",a*b);
+    printf("dev of 10 and 30   :  %f\n",(float)a/b);
+    printf("dev of 10 and 30   :  %.1f",(float)a/b);
+}
