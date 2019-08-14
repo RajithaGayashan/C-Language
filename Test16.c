221abd5f8abdc89eaf5e2b7f2e4a4a6af121966a
RajithaGayashan <rajithagayashan95@gmail.com>
2019-08-14 16:17:31 +0530

    Pointer-3

--- Test16.c
+++ Test16.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
-#include<string.h>
-int main(){
- char* name="Rajitha";
- int len;
- len=strlen(name);
- printf("%d\n",len); 	
+int main(int argc,char *argv[]){
+	int myArray[]={1,2,3};
+	get(myArray);
+	int *m=myArray;
+	get1(m);	
 }
-
+int get( int myArray[]){
+	printf("%d\n",myArray[1]);
+}
+void get1(int *p){
+	
+	p++;printf("%d",*p);
+}
