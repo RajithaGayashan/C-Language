fa093d90a6a56dbce0e6473834a44e91f8d29551
RajithaGayashan <rajithagayashan95@gmail.com>
2019-08-17 08:50:43 +0530

    gets & puts

---
+++ Test18.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+	
+	char array1[50];
+	printf("Enter your university : ");
+	gets(array1);
+	char array2[20];
+	printf("Enter your first name : ");
+	gets(array2);
+	char array3[20];
+	printf("Enter second name : ");
+	gets(array3);
+	puts(array1);
+	puts(array2);
+	printf("%s",array3);
+}
+
+
